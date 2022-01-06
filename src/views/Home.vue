30047b896f725a1eefd0812bd95b1e3dc6675719
Me06 <Rafael.walter@hs-weingarten.de>
2022-01-06 22:55:06 +0100

    Website erstellt

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="home">
+  <div :style="image" class="image">
+      <section class="head">
+        <div class="Logo">Breaking Bad Wiki</div>
+        <div class="input">
+          <input class="inputField" type="text" name="input" @keyup.enter="test2" />
+          <button class="inputButton" @click="loadapi()">Find Character</button>
+          </div>
+      </section>
+
+      <section class="randomCharacters">
+        <div class="container">
+          <div class="character1">
+           <img src="https://i.redd.it/hcprm17ktpu21.jpg" alt="DogPicture" width="280px" height="400px">
+          </div>
+          <div class="character2">
+           <img src="https://i.redd.it/hcprm17ktpu21.jpg" alt="DogPicture" width="280px" height="400px">
+          </div>
+          <div class="character3">
+           <img src="https://i.redd.it/hcprm17ktpu21.jpg" alt="DogPicture" width="280px" height="400px">
+          </div>
+          <div class="character4">
+            <img src="https://i.redd.it/hcprm17ktpu21.jpg" alt="DogPicture" width="280px" height="400px">
+          </div>
+          <div class="character5">
+            <img src="https://i.redd.it/hcprm17ktpu21.jpg" alt="DogPicture" width="280px" height="400px">
+          </div>
+          <div class="character6">
+            <img src="https://i.redd.it/hcprm17ktpu21.jpg" alt="DogPicture" width="280px" height="400px">
+          </div>
+          <div class="character7">
+            <img src="https://i.redd.it/hcprm17ktpu21.jpg" alt="DogPicture" width="280px" height="400px">
+          </div>
+          <div class="character8">
+            <img src="https://i.redd.it/hcprm17ktpu21.jpg" alt="DogPicture" width="280px" height="400px">
+          </div>
+        </div>
+      </section>
+
+</div>
+</div>
+    
+  
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
+  },
+
+
+  data: () => ({
+    image: { backgroundImage: "url(https://www.teahub.io/photos/full/72-721277_breaking-bad-4k-hd-desktop-wallpaper-for..jpg)" }
+
+  }),
+
+
+  methods: {
+    loadapi() {
+      console.log("button clicked");
+
+    }
   }
 }
+
 </script>
+
