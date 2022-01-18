f44ee6c16340db0077382a2036dacfca9e89df9d
Me06 <Rafael.walter@hs-weingarten.de>
2022-01-18 13:16:39 +0100

    random quote api fehler

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,9 @@
 <template>
 <div class="hello">
       <div class="Logo">Breaking Bad Wiki</div>
-  
- 
+      <ol class="randomQuote" v-for="random in quotes" :key="random.quote_id "></ol>
+         {{random.quote}}
+
 </div>
 
 
@@ -15,6 +16,37 @@
 export default {
 
   name: "Main",
+ 
+
+data(){
+    return {
+        quotes: [],
+        quote: "",
+        author:"",
+        quote_id: "",
+    };
+  },
+
+mounted(){
+      try{
+        fetch('https://www.breakingbadapi.com/api/quote/random')
+            .then((res)=> res.json())
+            .then((json)=> {
+                console.log(json);
+                this.quotes = json;
+            });
+
+        /*
+        const character  = await axios.get('https://www.breakingbadapi.com/api/characters/' , config);
+        //console.log(typeof(character.data));
+        this.list = character.data;
+        //console.log(typeof(this.list));
+      */}
+
+      catch(err){
+        console.log(err)
+      }
+    },
  
 }
 
